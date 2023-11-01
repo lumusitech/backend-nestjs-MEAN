f4779f5c5cad1ca2843d676b6ab509f05ca56fd3
lumusitech <lumusitech@gmail.com>
2023-10-31 21:27:00 -0300

    feat: add user schema

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { userSchema } from './entities/user.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: userSchema }])],
   controllers: [AuthController],
   providers: [AuthService],
 })
