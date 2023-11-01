5a4ce38171cdb53d75a92457ed8ab08b6bab55cc
lumusitech <lumusitech@gmail.com>
2023-10-31 22:47:20 -0300

    feat: add create user for post request

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    console.log(createUserDto);
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
 
-    return 'This action adds a new auth';
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcrypt.hashSync(password),
+        ...userData,
+      });
+
+      await newUser.save();
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: _, ...user } = newUser.toJSON();
+
+      return user;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists`);
+      }
+      throw new InternalServerErrorException('Something went wrong');
+    }
   }
 
   findAll() {
